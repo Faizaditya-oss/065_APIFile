16600e032d2087ab2d1d6196e0efa929cbd2be37
Faizaditya-oss <faizaditya750@gmail.com>
2026-08-10 10:20:10 +0700

    Menambahkan middleware upload menggunakan multer untuk menangani file gambar

---
+++ middleware/uploadMiddleware.js
@@ -0,0 +1,30 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+
+    filename:  (req, file, cb) => {
+        const uniqueName = Date.now() + '-' + file.originalname;
+        cb(null, uniqueName);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = /jpeg|jpg|png/;
+
+    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+    
+    const mimetype = allowedTypes.test(file.mimetype);
+
+    if (extname && mimetype) {
+        cb(null, true);
+    } else {
+        cb(new Error('Hanya file gambar yang diperbolehkan (jpeg, jpg, png).'));
+    }
+
+};
+
+    
